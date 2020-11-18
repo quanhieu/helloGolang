40bac8fad49063809e419b62d111c8239c0451ab
HieuTQ <truongquanghieu8198@gmail.com>
2020-11-18 22:59:33 +0700

    6. workspace p1

---
+++ 6.Workspace-p1/src/wth/helper/stringHelper.go
@@ -0,0 +1,7 @@
+package helper
+
+import "fmt"
+
+func ConvertStringToInt() {
+	fmt.Println("This String helper")
+}
---
+++ 6.Workspace-p1/src/wth/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	stringHelper "./helper"
+)
+
+func main() {
+	fmt.Println("This main function")
+
+	stringHelper.ConvertStringToInt()
+}
